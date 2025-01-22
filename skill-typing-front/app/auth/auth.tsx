f88cc1fbf8a81fccd378a4b479f8983b7573e89e
mametaro2024 <yuuiti726@gmail.com>
2025-01-22 13:54:10 +0900

    環境変数の修正漏れの対応

--- skill-typing-front/app/auth/auth.tsx
+++ skill-typing-front/app/auth/auth.tsx
@@ -18,9 +18,9 @@ function Auth() {
   const location = useLocation();
 
   const signOutRedirect = () => {
-    const clientId = "16thpun6fesvn6sp90jtou7kl5";
-    const logoutUri = "https://google.com";
-    const cognitoDomain = "https://ap-northeast-1bbs2wufvb.auth.ap-northeast-1.amazoncognito.com";
+    const clientId = import.meta.env.VITE_COGNITO_CLIENT_ID;
+    const logoutUri = import.meta.env.VITE_COGNITO_LOGOUT_URI;
+    const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
