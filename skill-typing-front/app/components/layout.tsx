5dcec8e2cacfb137918f49644508a018e30fff3f
Taka <takahito.uchino.1218@gmail.com>
2025-02-05 15:47:01 +0900

    [update]layoutコンポーネント修正
    layoutコンポーネントのルートを修正しました。

--- skill-typing-front/app/components/layout.tsx
+++ skill-typing-front/app/components/layout.tsx
@@ -4,11 +4,11 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <div>
+    <>
       <Header />
       <main>
         <Outlet />
       </main>
-    </div>
+    </>
   );
 }
